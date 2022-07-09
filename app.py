3169ea0f88ad738245610799f775a5498486f05d
Briskrem <crissjohndoe@gmail.com>
2022-07-09 08:58:41 -0400

    app changed

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import os
 import json
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///covid_db')
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///covid_db').replace("://", "ql://", 1)
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///covid_db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///covid_db').replace("://", "ql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECT'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY','heroku_push')
